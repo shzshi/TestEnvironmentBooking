1ae3005a1d5af001f516689e6f5f36de8e9ddefd
Shashikant Bangera <shzshi@gmail.com>
2015-03-24 16:33:36 +0000

    updated README.md & added .sql

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-TestEnvironmentBooking
+Test Environment Booking Tool 
+
+This in an opensource tool written in PHP with code-ignitor(MVC). you are free to clone, do a pull request or download and use it commercially. 
+
+This tool allows you to create environment, add multiple users, Reserve environment and admin user can approve,edit,delete the requests. 
+There is calendar view to see the current reserve. Currently I am not checking the environment booking overlap. 
+
+Demo : 
+http://www.indiadevops.com/environmentbooking/ 
+
+You are welcome to reach me with suggestion and idea to improve the tool. 
+
+PHP Version : 5.3.3
+Mysql : 5.1.73
+Browser : Chrome and IE-9 
+
+git clone https://github.com/shzshi/TestEnvironmentBooking.git
+
+Edit config.php in directory application/config/  
+$config['base_url'] = http://www.myenvironmentbooking.com
+
+Edit database.php in directory application/config/
+$db['default']['hostname'] = 'mysql-hostname';
+$db['default']['username'] = 'mysql-username';
+$db['default']['password'] = 'mysql-password';
+$db['default']['database'] = 'mysql-database';
+$db['default']['dbdriver'] = 'mysql';
+
+edit .htaccess
+RewriteBase /environmentbooking/
+php_value error_log  /var/www/html/environmentbooking/PHP_errors.log
--- testenvironmentbooking.sql
+++ testenvironmentbooking.sql
@@ -10,13 +10,13 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping database structure for testenvironmentbooking
-DROP DATABASE IF EXISTS `testenvironmentbooking`;
-CREATE DATABASE IF NOT EXISTS `testenvironmentbooking` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `testenvironmentbooking`;
+-- Dumping database structure for environmentbooking
+DROP DATABASE IF EXISTS `environmentbooking`;
+CREATE DATABASE IF NOT EXISTS `environmentbooking` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `environmentbooking`;
 
 
--- Dumping structure for table testenvironmentbooking.calendar
+-- Dumping structure for table environmentbooking.calendar
 DROP TABLE IF EXISTS `calendar`;
 CREATE TABLE IF NOT EXISTS `calendar` (
   `calendarid` int(10) NOT NULL AUTO_INCREMENT,
@@ -31,20 +31,12 @@ CREATE TABLE IF NOT EXISTS `calendar` (
   PRIMARY KEY (`calendarid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
--- Dumping data for table testenvironmentbooking.calendar: 6 rows
+-- Dumping data for table environmentbooking.calendar: 6 rows
 DELETE FROM `calendar`;
 /*!40000 ALTER TABLE `calendar` DISABLE KEYS */;
-INSERT INTO `calendar` (`calendarid`, `reservename`, `reservetype`, `envid`, `starttime`, `endtime`, `status`, `createdon`, `createdby`) VALUES
-	(18, 'Release2.0', 'generic', 6, '2015-03-17', '2015-03-19', 'approved', '2015-03-15 06:40:07', 4),
-	(15, 'Reservation1', 'generic', 6, '2015-03-18', '2015-03-19', 'approved', '2015-03-15 06:39:21', 4),
-	(16, 'Release1.0', 'generic', 6, '2015-03-15', '2015-03-16', 'in-progress', '2015-03-15 06:39:36', 4),
-	(14, 'New Reservation', 'release', 5, '2015-03-24', '2015-03-17', 'in-progress', '2015-03-14 12:26:58', 4),
-	(19, 'Reservation22323', 'release', 6, '2015-03-18', '2015-03-20', 'approved', '2015-03-15 06:40:31', 4),
-	(20, 'Deployment Jabbix', 'maintainance', 12, '2015-03-22', '2015-03-24', 'approved', '2015-03-22 05:59:44', 6);
-/*!40000 ALTER TABLE `calendar` ENABLE KEYS */;
 
 
--- Dumping structure for table testenvironmentbooking.environment
+-- Dumping structure for table environmentbooking.environment
 DROP TABLE IF EXISTS `environment`;
 CREATE TABLE IF NOT EXISTS `environment` (
   `envid` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Environment id',
@@ -55,23 +47,13 @@ CREATE TABLE IF NOT EXISTS `environment` (
   PRIMARY KEY (`envid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
--- Dumping data for table testenvironmentbooking.environment: 9 rows
+-- Dumping data for table environmentbooking.environment: 9 rows
 DELETE FROM `environment`;
 /*!40000 ALTER TABLE `environment` DISABLE KEYS */;
-INSERT INTO `environment` (`envid`, `envname`, `envtype`, `createdon`, `createdby`) VALUES
-	(6, 'Joomla', 'development', '2015-03-12', 4),
-	(5, 'Drupal', 'development', '2015-03-12', 4),
-	(7, 'Drupal', 'non-production', '2015-03-12', 4),
-	(8, 'Drupal', 'production', '2015-03-12', 4),
-	(9, 'Joomla', 'non-production', '2015-03-12', 4),
-	(11, 'Joomla', 'production', '2015-03-12', 4),
-	(12, 'WordPress', 'development', '2015-03-12', 4),
-	(13, 'WordPress', 'non-production', '2015-03-12', 4),
-	(14, 'WordPress', 'production', '2015-03-12', 4);
 /*!40000 ALTER TABLE `environment` ENABLE KEYS */;
 
 
--- Dumping structure for table testenvironmentbooking.users
+-- Dumping structure for table environmentbooking.users
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `userid` int(10) NOT NULL AUTO_INCREMENT,
@@ -90,12 +72,10 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`userid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Dumping data for table testenvironmentbooking.users: 3 rows
+-- Dumping data for table environmentbooking.users: 3 rows
 DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`userid`, `username`, `password`, `firstname`, `lastname`, `usertype`, `organization`, `designation`, `securityquestion`, `securityanswer`, `phone`, `createdby`, `createdon`) VALUES
 	(1, 'admin@localhost.com', '*89FA6EAF8B6264AC8D6E84759027252505A3EAEE', 'Administrator', 'Administrator', 'admin', 'Nemu Hengsu Technologies', 'CEO', 'What is your first pet name?', 'Rocky', 2147483647, 0, '2015-03-17 16:26:12');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
