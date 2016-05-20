601da0069443fc83935538e91193370e6cac9807
root <root@lamp.(none)>
2016-05-20 13:23:46 +0000

    remove logs files

--- application/logs/log-2016-05-10.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2016-05-10 15:56:34 --> Severity: Notice  --> Undefined property: CI_Session::$usertype /var/www/html/TestEnvironmentBooking/application/controllers/user.php 30
-ERROR - 2016-05-10 15:57:10 --> Severity: Notice  --> Undefined property: CI_Session::$usertype /var/www/html/TestEnvironmentBooking/application/controllers/user.php 30
-ERROR - 2016-05-10 15:57:34 --> Severity: Notice  --> Undefined property: CI_Session::$usertype /var/www/html/TestEnvironmentBooking/application/controllers/user.php 30
